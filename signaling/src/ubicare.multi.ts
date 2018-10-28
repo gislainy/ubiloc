ee57456d9d93b390e46908e09db9140c87a3d4f8
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2018-10-28 16:09:14 -0200

    multi

--- signaling/src/ubicare.multi.ts
+++ signaling/src/ubicare.multi.ts
@@ -6,7 +6,15 @@ enum MessageType {
   MATCHED = 'matched',
   SDP = 'sdp',
   ICE = 'ice',
-  PEER_LEFT = 'peer-left'
+  PEER_LEFT = 'peer-left',
+  LOCATOR = 'locator',
+  MONITOR = 'monitor'
+}
+
+enum SessionType {
+  LOCATOR = 'locator',
+  MONITOR = 'monitor'
+  
 }
 
 type MatchMessage = {
@@ -30,28 +38,41 @@ type ICEMessage = {
 type PeerLeft = {
   type: MessageType.PEER_LEFT
 }
+type LocatorMessage = {
+  type: MessageType.LOCATOR,
+  permission: string,
+}
+type MonitorMessage = {
+  type: MessageType.MONITOR,
+  permission: string,
+}
 
 type ClientMessage
   = MatchMessage
   | SDPMessage
   | ICEMessage
   | PeerLeft
+  | LocatorMessage
+  | MonitorMessage
 
 type Session = {
   id: string
   ws: WebSocket,
-
+  type?: string,
+  permission?: SessionType,
   peer?: Array<string>
 }
 
-export default class Roulette {
+export default class Ubicare {
 
   private sessions: Map<string, Session>;
-  private unmatched: Array<string>;
+  private locator: Array<string>;
+  private monitor: Array<string>;
 
   constructor() {
     this.sessions = new Map();
-    this.unmatched = [];
+    this.locator = [];
+    this.monitor = [];
   }
 
   register(ws: WebSocket) {
@@ -60,7 +81,7 @@ export default class Roulette {
     const session = { id, ws, peer };
 
     this.sessions.set(id, session);
-    this.tryMatch(session);
+    //this.tryMatch(session);
 
     ws.on('close', () => this.unregister(id));
     ws.on('error', () => this.unregister(id));
@@ -70,12 +91,20 @@ export default class Roulette {
   private handleMessage(id: string, data: string) {
     try {
       const message = JSON.parse(data) as ClientMessage;
+      console.log("handleMessage", message);
       const session = this.sessions.get(id);
       if (!session) { return console.error(`Can't find session for ${id}`); }
       switch (message.type) {
+        case MessageType.LOCATOR:
+          this.handleLocator(id, data);
+          break;
+        case MessageType.MONITOR:
+          this.handleMonitor(id, data);
+          break;
         case MessageType.SDP:
         case MessageType.ICE:
           session.peer.forEach(pId => {
+            console.log({peerId: pId, message});
             const peer = this.sessions.get(pId);
             if (!peer) { return console.error(`Can't find session for peer of ${id}`); }
             this.send(peer, message);
@@ -92,30 +121,31 @@ export default class Roulette {
     }
   }
 
-  private tryMatch(session: Session) {
-    if (this.unmatched.length > 0) {
-      const match = this.unmatched[0];
-      const other = this.sessions.get(match);
-      if (other) {
-        session.peer.push(match);
-        other.peer.push(session.id);
-        this.send(session, { type: MessageType.MATCHED, match: other.id, offer: true });
-        this.send(other, { type: MessageType.MATCHED, match: session.id, offer: true });
-      }
-    } else {
-      this.unmatched.push(session.id);
-    }
-  }
+  // private tryMatch(session: Session) {
+  //   if (this.unmatched.length > 0) {
+  //     const match = this.unmatched[0];
+  //     const other = this.sessions.get(match);
+  //     if (other) {
+  //       session.peer.push(match);
+  //       other.peer.push(session.id);
+  //       this.send(session, { type: MessageType.MATCHED, match: other.id, offer: true });
+  //       this.send(other, { type: MessageType.MATCHED, match: session.id, offer: true });
+  //     }
+  //   } else {
+  //     this.unmatched.push(session.id);
+  //   }
+  // }
 
   private unregister(id: string) {
     const session = this.sessions.get(id);
     if (session && session.peer) {
       session.peer.forEach(pId => {
         const peer = this.sessions.get(pId);
-        if (peer) this.send(peer, { type: MessageType.PEER_LEFT })
+        if (peer) this.send(peer, { type: MessageType.PEER_LEFT });
       })
     }
-    this.unmatched = this.unmatched.filter(other => id !== other);
+    this.monitor = this.monitor.filter(other => id !== other);
+    this.locator = this.locator.filter(other => id !== other);
     this.sessions.delete(id);
   }
 
@@ -129,4 +159,29 @@ export default class Roulette {
     }
   }
 
+  private handleMonitor(id: string, data: string) {
+    const message = JSON.parse(data);
+    console.log('handleMonitor', arguments);
+    const monitor = this.sessions.get(id);
+    monitor.type = SessionType.LOCATOR;
+    monitor.permission = message.permission;
+    this.monitor.push(id);
+    this.locator.forEach(mId => {
+      const locator = this.sessions.get(mId);
+      if(locator.permission == message.permission) {
+        monitor.peer.push(mId);
+        locator.peer.push(id);
+        this.send(monitor, { type: MessageType.MATCHED, match: locator.id, offer: false });
+        this.send(locator, { type: MessageType.MATCHED, match: monitor.id, offer: true });
+      }
+    });
+  }
+  private handleLocator(id: string, data: any) {
+    const message = JSON.parse(data);
+    console.log('handleLocator', arguments);
+    const locator = this.sessions.get(id);
+    locator.type = SessionType.LOCATOR;
+    locator.permission = message.permission;
+    this.locator.push(id);
+  }
 }
